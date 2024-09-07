1fd764be4eb6f85da6882547997cbacc6c9fae32
Anngin <157882688+Anngin@users.noreply.github.com>
2024-09-07 12:25:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+//паралакс
+    var elems2 = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems2);
+  });
+
+
+
+
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems);
+  });
+
+
+
+
+
